ad70c54025f522dc7808f177e693dd07eeb11f15
Mukesh2312 <49585682+Mukesh2312@users.noreply.github.com>
2025-10-24 21:02:31 +0530

    setup navbar footer

---
+++ src/components/Body.jsx
@@ -0,0 +1,13 @@
+import Footer from "./Footer";
+import NavBar from "./NavBar";
+
+const Body = () => {
+  return (
+    <div>
+      <NavBar />
+      <Footer />
+    </div>
+  );
+};
+
+export default Body;
